776c9656ec6f476f543cd4e9eff9b4d835ac64b4
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2017-11-27 15:23:26 -0600

    Lasso for yll_abs

--- Project5_Stefanos.R
+++ Project5_Stefanos.R
@@ -37,17 +37,24 @@ summary(boost.df)
 
 ### Lasso
 df1=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -yll_abs_ui_upto, -yll_abs_ui_from)
+df1=dplyr::sample_n(df1, 10000)
+
+df1_nona <- na.omit(df1)
+
+attach(df1_nona)
+
 # for this you have to pass in a matrix of x. You have to construct the x's
-x=model.matrix(yll_abs~.-1,data=df1) 
+x=model.matrix(yll_abs~.-1,data=df1_nona) 
 # and construct the y's
-y=df1$yll_abs
-?glmnet
-fit.lasso=glmnet(x,y,family="binomial")
+y=df1_nona$yll_abs
+
+fit.lasso=glmnet(x,y,family="gaussian")
 # plot how lambda is changing against the model fit
 plot(fit.lasso,xvar="lambda",label=TRUE) # the one that disappears last is our best predictor
 # do cross validation for each lambda (default is kfold of 10 across 100 lambdas)
-cv.lasso=cv.glmnet(x,y,family="binomial")
+cv.lasso=cv.glmnet(x,y,family="gaussian")
 # plot the cross validation mean squared errors of all 100 models
 plot(cv.lasso)
 # get the coefficients for what it thinks is the best model
 coef(cv.lasso)
+
