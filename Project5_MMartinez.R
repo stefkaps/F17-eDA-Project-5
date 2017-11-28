9ae4a059194fc8a0fc8c8c24f5550988244359d6
Marcus Gabriel Martinez <marcusgmartinez@utexas.edu>
2017-11-27 23:57:08 -0600

    New linear regression and associated image.

--- Project5_MMartinez.R
+++ Project5_MMartinez.R
@@ -44,12 +44,28 @@ plot(regfit.full,scale="adjr2")
 coef(regfit.full,10)
 
 #deathabs section#
-dfb=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-dfb=dplyr::sample_n(dfb, 10000)
+df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_numeric=dplyr::sample_n(df_numeric, 10000)
 
-attach(dfb)
+attach(df_numeric)
 
-regfit.full=regsubsets(death_abs~.,data=dfb, nvmax=37)
+regfit.full=regsubsets(death_abs~.,data=df_numeric, nvmax=37)
+reg.summary=summary(regfit.full)
+names(reg.summary)
+plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
+which.min(reg.summary$cp)
+
+plot(reg.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
+which.max(reg.summary$adjr2)
+
+summary(regfit.full)
+
+plot(regfit.full,scale="Cp")
+plot(regfit.full,scale="adjr2")
+coef(regfit.full,10)
+
+#region_name section# too big???
+regfit.full=regsubsets(as.factor(region_name)~.,data=df, nvmax=37)
 reg.summary=summary(regfit.full)
 names(reg.summary)
 plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
@@ -65,13 +81,13 @@ plot(regfit.full,scale="adjr2")
 coef(regfit.full,10)
 
 ##Forwards and Backwards Selection Section##
-dfb=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-dfb=dplyr::sample_n(dfb, 10000)
+df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_numeric=dplyr::sample_n(df_numeric, 10000)
 
-attach(dfb)
+attach(df_numeric)
 
 #Forward#
-regfit.fwd=regsubsets(death_abs~.,data=dfb,nvmax=37,method="forward")
+regfit.fwd=regsubsets(death_abs~.,data=df_numeric,nvmax=37,method="forward")
 summary(regfit.fwd)
 
 plot(regfit.fwd,scale="Cp")
@@ -86,7 +102,7 @@ plot(regfwd.summary$cp,xlab="Number of Variables",ylab="Cp")
 plot(regfwd.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
 
 #Backward#
-regfit.bwd=regsubsets(death_abs~.,data=dfb,nvmax=37,method="backward")
+regfit.bwd=regsubsets(death_abs~.,data=df_numeric,nvmax=37,method="backward")
 summary(regfit.bwd)
 
 plot(regfit.bwd,scale="Cp")
@@ -120,32 +136,46 @@ table(knn.pred,cause_name[test_knn])
 mean(knn.pred==cause_name[test_knn])
 
 ##Linear Regression Section##
-dfb=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-dfb=dplyr::sample_n(dfb, 10000)
+df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_numeric=dplyr::sample_n(df_numeric, 10000)
 
-attach(dfb)
+#attach(df_numeric)
+
+df_numeric_nona = na.omit(df_numeric)
+attach(df_numeric_nona)
 
 #Original Predictors#
-fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs + yll_abs_ui_upto,data=dfb)
+fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs + yll_abs_ui_upto,data=df_numeric)
 fitDeathAbs
 summary(fitDeathAbs)
 
 #Updated Predictors#
-fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs,data=dfb)
+fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs,data=df_numeric)
 fitDeathAbs
 summary(fitDeathAbs)
 
+#yll_abs#
+df_yll_abs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit)
+df_yll_abs=dplyr::sample_n(df_yll_abs, 10000)
+attach(df_yll_abs)
+df_nona_yll = na.omit(df_yll_abs)
+attach(df_nona_yll)
+
+fitYLLAbs = lm(yll_abs~death_abs_ui_upto + daly_abs + daly_abs_ui_upto + daly_abs_ui_from,data=df_yll_abs)
+fitYLLAbs
+summary(fitYLLAbs)
+
 ##Lasso Section##
-dfb=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-dfb=dplyr::sample_n(dfb, 10000)
+df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_numeric=dplyr::sample_n(df_numeric, 10000)
 
-dfb_nona <- na.omit(dfb)
+df_numeric_nona <- na.omit(df_numeric)
 
-attach(dfb_nona)
+attach(df_numeric_nona)
 
-x=model.matrix(death_abs~.-1,data=dfb_nona) 
+x=model.matrix(death_abs~.-1,data=df_numeric_nona) 
 
-y=dfb_nona$death_abs
+y=df_numeric_nona$death_abs
 
 fit.lasso=glmnet(x,y,family="gaussian")
 
@@ -156,6 +186,8 @@ cv.lasso=cv.glmnet(x,y,family="gaussian")
 plot(cv.lasso)
 
 coef(cv.lasso)
+
+
 
 
 
