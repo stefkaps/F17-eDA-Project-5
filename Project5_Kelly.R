86f7c7b33a1261bd3cf841c0ec52d69ce64eff5f
Kelly <kellyjennings@utexas.edu>
2017-11-30 23:20:31 -0600

    svm

--- Project5_Kelly.R
+++ Project5_Kelly.R
@@ -1,7 +1,3 @@
-library(tidyverse)
-library(modelr)
-require(dplyr)
-require(data.world)
 require(MASS)
 require(ISLR)
 require(ggplot2)
@@ -10,6 +6,11 @@ library(glmnet)
 require(tree)
 require(randomForest)
 require(gbm)
+require(e1071)
+library(tidyverse)
+library(modelr)
+require(dplyr)
+require(data.world)
 
 data.world::set_config(save_config(auth_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwcm9kLXVzZXItY2xpZW50Om1hcmN1c2dhYmUtdXQiLCJpc3MiOiJhZ2VudDptYXJjdXNnYWJlLXV0OjowYjE2NDQzOC1mYzRlLTRhNDktYWY1MC1iMWU1YjViYmIzYzMiLCJpYXQiOjE0ODQ4NjgyNjMsInJvbGUiOlsidXNlcl9hcGlfcmVhZCIsInVzZXJfYXBpX3dyaXRlIl0sImdlbmVyYWwtcHVycG9zZSI6dHJ1ZX0.Eb9i31mYAv6zQGjlze-PbiBJ_5_JNBDIZn51wcPnnNPny_ih2SSN9Ur_LVyRltEbrReNXM5b371XWrmMiexEKw"))
 #vignette("quickstart", package = "data.world")
@@ -27,13 +28,8 @@ boost.df=gbm(death_abs~.,data=dfb,distribution="gaussian",n.trees=500,shrinkage=
 summary(boost.df)
 
 # k means clustering
-dfe <- data.world::query(
-  data.world::qry_sql("SELECT * FROM GlobalBurdenofDisease_Europe"),
-  dataset = project
-)
 
-dfe_heart = dfe %>% dplyr::filter(region_name %in% c("Eastern Europe", "Western Europe") & cause_name %in% c("Ischemic heart disease", "Ischemic stroke") & age_name_unit == "years")
-
+dfe_heart = df %>% dplyr::filter(region_name %in% c("Eastern Europe", "Western Europe") & cause_name %in% c("Ischemic heart disease", "Ischemic stroke") & age_name_unit == "years")
 x_km = dfe_heart%>%dplyr::select(yld_rate, yll_rate)
 km.out=kmeans(x_km,2)
 km.out
@@ -50,8 +46,76 @@ kmeansubject <- dfcluster2 %>% ggplot(aes(x=yld_rate, y=yll_rate, colour = as.fa
 actualsubject <- dfcluster2 %>% ggplot(aes(x=yld_rate, y=yll_rate, colour = as.factor(dfe_heart.region_name))) + geom_point()
 
 df1 <- data.frame(km.out$centers, km.out$size)
-names(df1)
+#names(df1)
 dfcluster2 %>% ggplot() + geom_point(mapping = aes(x=yld_rate, y=yll_rate, colour = as.factor(km.out.cluster))) + geom_point(data=df1, mapping=aes(yld_rate, yll_rate, size=km.out.size))
 
 dfe_heart %>% ggplot(aes(x=yld_rate, y = yll_rate, colour = region_name)) + geom_point()
+
+# hierchical 
+hc.complete=hclust(dist(x_km),method="complete")
+hc.single=hclust(dist(x_km),method="single")
+hc.average=hclust(dist(x_km),method="average")
+par(mfrow = c(3, 1))
+plot(hc.complete)
+plot(hc.single)
+plot(hc.average)
+
+hc.cut1=cutree(hc.complete,2)
+hc.cut2=cutree(hc.single,2)
+hc.cut3=cutree(hc.average,2)
+
+table(hc.cut1,dfe_heart$region_name)
+table(hc.cut1,km.out$cluster)
+
+table(hc.cut2,dfe_heart$region_name)
+table(hc.cut2,km.out$cluster)
+
+table(hc.cut3,dfe_heart$region_name)
+table(hc.cut3,km.out$cluster)
+
+par(mfrow = c(1, 3))
+plot(x_km,col=hc.cut1,cex=2,main = "Hierarchical: Complete", pch=1,lwd=2)
+plot(x_km,col=hc.cut2,cex=2,main = "Hierarchical: Single", pch=1,lwd=2)
+plot(x_km,col=hc.cut3,cex=2,main = "Hierarchical: Average", pch=1,lwd=2)
+
+##### svm
+dfe_ms = df %>% dplyr::filter(region_name %in% c("Eastern Europe", "Western Europe") & sex_name %in% c("Male", "Female") & cause_name %in% c("Multiple sclerosis") & age_name_unit == "years")
+
+x_try=subset(dfe_ms, select = c(death_rate, yld_rate))
+x_svm=matrix(unlist(x_try), ncol = 2)
+y_svm=dfe_ms$sex_name
+dat=data.frame(x_svm,y_svm)
+svmfit=svm(y_svm~.,data=dat,type="C-classification",kernel="linear",cost=10,scale=FALSE)
+print(svmfit)
+plot(svmfit,dat)
+make.grid=function(x,n=100){
+  grange=apply(x,2,range)
+  x1=seq(from=grange[1,1],to=grange[2,1],length=n)
+  x2=seq(from=grange[1,2],to=grange[2,2],length=n)
+  expand.grid(X1=x1,X2=x2)
+}
+xgrid=make.grid(x_svm)
+ygrid=predict(svmfit,xgrid)
+
+beta=drop(t(svmfit$coefs)%*%x_svm[svmfit$index,])
+beta0=svmfit$rho
+plot(xgrid,col=c("yellow","blue")[as.numeric(ygrid)],pch=20,cex=.2)
+tonum=function(ysvm){
+  tonumberdf = dfe_ms %>% dplyr::mutate(sex_name2 = ifelse(sex_name == "Male", 1, 0))
+  tonumberdf$sex_name2
+}
+points(x_svm,col=tonum(df)+2,pch=19)
+points(x_svm[svmfit$index,],pch=5,cex=2)
+abline(beta0/beta[2],-beta[1]/beta[2])
+abline((beta0-1)/beta[2],-beta[1]/beta[2],lty=2)
+abline((beta0+1)/beta[2],-beta[1]/beta[2],lty=2)
+
+x_try2=subset(dfe_ms, select = c(yld_rate, death_rate))
+x_svm2=matrix(unlist(x_try2), ncol = 2)
+dat2=data.frame(x_svm2,y_svm)
+nlsvmfit=svm(y_svm~.,data=dat2,type="nu-classification",scale=TRUE,kernel="radial",cost=10)
+plot(nlsvmfit, dat2)
+
+
+
 
