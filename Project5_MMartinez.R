f03d334f9b529042ebe648dc58d8c57efd5681ab
Marcus Gabriel Martinez <marcusgmartinez@utexas.edu>
2017-12-01 00:28:02 -0600

    Addition of Random Forest and associated images.

--- Project5_MMartinez.R
+++ Project5_MMartinez.R
@@ -44,12 +44,12 @@ plot(regfit.full,scale="adjr2")
 coef(regfit.full,10)
 
 #deathabs section#
-df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-df_numeric=dplyr::sample_n(df_numeric, 10000)
+df_deathabs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_deathabs=dplyr::sample_n(df_deathabs, 10000)
 
-attach(df_numeric)
+attach(df_deathabs)
 
-regfit.full=regsubsets(death_abs~.,data=df_numeric, nvmax=37)
+regfit.full=regsubsets(death_abs~.,data=df_deathabs, nvmax=37)
 reg.summary=summary(regfit.full)
 names(reg.summary)
 plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp")
@@ -65,13 +65,13 @@ plot(regfit.full,scale="adjr2")
 coef(regfit.full,10)
 
 ##Forwards and Backwards Selection Section##
-df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-df_numeric=dplyr::sample_n(df_numeric, 10000)
+df_deathabs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_deathabs=dplyr::sample_n(df_deathabs, 10000)
 
-attach(df_numeric)
+attach(df_deathabs)
 
 #Forward#
-regfit.fwd=regsubsets(death_abs~.,data=df_numeric,nvmax=37,method="forward")
+regfit.fwd=regsubsets(death_abs~.,data=df_deathabs,nvmax=37,method="forward")
 summary(regfit.fwd)
 
 plot(regfit.fwd,scale="Cp")
@@ -108,7 +108,7 @@ plot(regfwd.summary$adjr2,xlab="Number of Variables",ylab="adjr2")
 
 
 #Backward#
-regfit.bwd=regsubsets(death_abs~.,data=df_numeric,nvmax=37,method="backward")
+regfit.bwd=regsubsets(death_abs~.,data=df_deathabs,nvmax=37,method="backward")
 summary(regfit.bwd)
 
 plot(regfit.bwd,scale="Cp")
@@ -142,21 +142,21 @@ table(knn.pred,cause_name[test_knn])
 mean(knn.pred==cause_name[test_knn])
 
 ##Linear Regression Section##
-df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-df_numeric=dplyr::sample_n(df_numeric, 10000)
+df_deathabs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_deathabs=dplyr::sample_n(df_deathabs, 10000)
 
-#attach(df_numeric)
+#attach(df_deathabs)
 
-df_numeric_nona = na.omit(df_numeric)
-attach(df_numeric_nona)
+df_deathabs_nona = na.omit(df_deathabs)
+attach(df_deathabs_nona)
 
 #Original Predictors#
-fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs + yll_abs_ui_upto,data=df_numeric)
+fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs + yll_abs_ui_upto,data=df_deathabs)
 fitDeathAbs
 summary(fitDeathAbs)
 
 #Updated Predictors#
-fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs,data=df_numeric)
+fitDeathAbs = lm(death_abs~yll_abs_ui_from + yll_abs,data=df_deathabs)
 fitDeathAbs
 summary(fitDeathAbs)
 
@@ -172,16 +172,16 @@ fitYLLAbs
 summary(fitYLLAbs)
 
 ##Lasso Section##
-df_numeric=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
-df_numeric=dplyr::sample_n(df_numeric, 10000)
+df_deathabs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+df_deathabs=dplyr::sample_n(df_deathabs, 10000)
 
-df_numeric_nona <- na.omit(df_numeric)
+df_deathabs_nona <- na.omit(df_deathabs)
 
-attach(df_numeric_nona)
+attach(df_deathabs_nona)
 
-x=model.matrix(death_abs~.-1,data=df_numeric_nona) 
+x=model.matrix(death_abs~.-1,data=df_deathabs_nona) 
 
-y=df_numeric_nona$death_abs
+y=df_deathabs_nona$death_abs
 
 fit.lasso=glmnet(x,y,family="gaussian")
 
@@ -231,5 +231,70 @@ pca.out=prcomp(df_nona2, scale=TRUE)
 pca.out
 names(pca.out)
 biplot(pca.out, scale=0)
+
+##Logistic Regression Section## Keep getting 0 for mean correct. 
+df_subset <- df %>% dplyr::select(., -cause_medium, -cause_short, -region_name, -year, -age_name_unit, -sex_name)
+df_nona <- na.omit(df_subset)
+attach(df_nona)
+
+set.seed(1)
+train = sample(nrow(df_nona), 33346)
+test = df_nona[-train,]
+
+glm1.fit=glm(as.factor(cause_name) ~ age_name_from + death_abs + death_abs_ui_upto + death_pct_ui_from + death_pct_ui_upto + death_rate + death_rate_ui_from + yll_abs + yll_abs_ui_from + yll_abs_ui_upto + yll_rate + yll_rate_ui_from + yld_abs + yld_abs_ui_from + yld_pct + yld_pct_ui_from + yld_pct_ui_upto + yld_rate + yld_rate_ui_upto + daly_abs + daly_abs_ui_from + daly_abs_ui_upto + daly_pct + daly_rate + daly_rate_ui_from,
+             data=df_nona, family=binomial,
+             subset=train)
+summary(glm1.fit)
+
+glm1.probs=predict(glm1.fit,newdata=test,type="response")
+glm1.pred=ifelse(glm1.probs>0.5,"1","0")
+cause_name.test = test$cause_name
+table(glm1.pred,cause_name.test)
+mean(glm1.pred==cause_name.test)
+
+##LDA Section## Not working 
+lda1.fit=lda(cause_name ~ age_name_from + death_abs + death_abs_ui_upto + death_pct_ui_from + death_pct_ui_upto + death_rate + death_rate_ui_from + yll_abs + yll_abs_ui_from + yll_abs_ui_upto + yll_rate + yll_rate_ui_from + yld_abs + yld_abs_ui_from + yld_pct + yld_pct_ui_from + yld_pct_ui_upto + yld_rate + yld_rate_ui_upto + daly_abs + daly_abs_ui_from + daly_abs_ui_upto + daly_pct + daly_rate + daly_rate_ui_from,
+             data=df_subset, subset=train)
+lda1.fit
+lda1.pred=predict(lda1.fit, test)
+lda1_df = data.frame(lda1.pred)
+table(lda1.pred$class,test$cause_name)
+mean(lda1.pred$class==test$cause_name)
+
+##QDA Section## Not working
+qda1.fit = qda(cause_name ~ age_name_from + death_abs + death_abs_ui_upto + death_pct_ui_from + death_pct_ui_upto + death_rate + death_rate_ui_from + yll_abs + yll_abs_ui_from + yll_abs_ui_upto + yll_rate + yll_rate_ui_from + yld_abs + yld_abs_ui_from + yld_pct + yld_pct_ui_from + yld_pct_ui_upto + yld_rate + yld_rate_ui_upto + daly_abs + daly_abs_ui_from + daly_abs_ui_upto + daly_pct + daly_rate + daly_rate_ui_from,
+               data=df_subset, subset=train)
+qda1.fit
+qda1.pred = predict(qda1.fit, test)
+table(qda1.pred$class,test$cause_name)
+mean(qda1.pred$class==test$cause_name)
+
+##Random Forests Section##
+df_deathabs=dplyr::select(df, -cause_name, -cause_medium, -cause_short, -region_name, -sex_name, -age_name_unit, -death_abs_ui_upto, -death_abs_ui_from)
+set.seed(11)
+df_deathabs=dplyr::sample_n(df_deathabs, 10000)
+df_deathabs_nona = na.omit(df_deathabs)
+attach(df_deathabs_nona)
+
+set.seed(101)
+dim(df_deathabs_nona)
+train=sample(1:nrow(df_deathabs_nona),4457)
+
+rf.death=randomForest(death_abs~.,data=df_deathabs_nona,subset=train)
+rf.death
+
+oob.err=double(37)
+test.err=double(37)
+for(mtry in 1:37){
+  fit=randomForest(death_abs~.,data=df_deathabs_nona,subset=train,mtry=mtry,ntree=400)
+  oob.err[mtry]=fit$mse[400]
+  pred=predict(fit,df_deathabs_nona[-train,])
+  test.err[mtry]=with(df_deathabs_nona[-train,],mean((death_abs-pred)^2))
+  cat(mtry," ")
+}
+matplot(1:mtry,cbind(test.err,oob.err),pch=19,col=c("red","blue"),type="b",ylab="Mean Squared Error")
+legend("topright",legend=c("OOB","Test"),pch=19,col=c("red","blue"))
+
+
 
 
